7f574fdd4d7a2d8967bc800a6e1621ea631e47d2
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-11-03 17:59:37 +0200

    header is creating

---
+++ resources/views/main.blade.php
@@ -0,0 +1,102 @@
+<x-base>
+    <div class="header align-items-end">
+        <ul
+            class="
+                header__list
+                d-flex
+                flex-row
+                justify-content-end
+                align-items-center
+            "
+        >
+            <li class="header__list__item">
+                Home
+            </li>
+
+            <select class="header__list__item header__list__item__select">
+                <option
+                    class="
+                        header__list__item__option
+                        header__list__item__option--first
+                    "
+                    value="option1"
+                    hidden
+                >
+                    My Account
+                </option>
+                <option value="option2">Documentation</option>
+                <option value="option3">Discover</option>
+                <option value="option4">Your LevitAI World</option>
+                <option value="option4">Community</option>
+            </select>
+
+            <select class="header__list__item header__list__item__select">
+                <option
+                    class="
+                        header__list__item__option
+                        header__list__item__option--first
+                    "
+                    value="option1"
+                    hidden
+                >
+                    Documentation
+                </option>
+                <option value="option2">My Account</option>
+                <option value="option3">Discover</option>
+                <option value="option4">Your LevitAI World</option>
+                <option value="option4">Community</option>
+            </select>
+
+            <select class="header__list__item header__list__item__select">
+                <option
+                    class="
+                        header__list__item__option
+                        header__list__item__option--first
+                    "
+                    value="option1"
+                    hidden
+                >
+                    Discover
+                </option>
+                <option value="option2">My Account</option>
+                <option value="option3">Documentation</option>
+                <option value="option4">Your LevitAI World</option>
+                <option value="option5">Community</option>
+            </select>
+
+            <select class="header__list__item header__list__item__select">
+                <option
+                    class="
+                        header__list__item__option
+                        header__list__item__option--first
+                    "
+                    value="option1"
+                    hidden
+                >
+                    Your LevitAI World
+                </option>
+                <option value="option2">My Account</option>
+                <option value="option3">Documentation</option>
+                <option value="option4">Discover</option>
+                <option value="option5">Community</option>
+            </select>
+
+            <select class="header__list__item header__list__item__select">
+                <option
+                    class="
+                        header__list__item__option
+                        header__list__item__option--first
+                    "
+                    value="option1"
+                    hidden
+                >
+                    Community
+                </option>
+                <option value="option2">My Account</option>
+                <option value="option3">Documentation</option>
+                <option value="option4">Discover</option>
+                <option value="option5">Your LevitAI World</option>
+            </select>
+        </ul>
+    </div>
+</x-base>
