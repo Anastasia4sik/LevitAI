02dc7e21b49935069134cc941357699a021d6292
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-11-20 17:10:40 +0200

    available block created

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1135,6 +1135,27 @@
     </div>
   </section>
 
+  <section class="available text-center">
+    <h2 class="available__title title-main">
+        Also Available <span class=" title-main--stroke">LevitAI</span> <br>
+        On <span class=" title-main--stroke">IOS</span> And <span class=" title-main--stroke">Android</span>
+    </h2>
+
+    <h3 class="available__desk title-little">
+        We work for this task
+    </h3>
+
+    <div class="available__links d-flex flex-row justify-content-center">
+        <a target="_blank" href="https://www.apple.com/app-store/" class="available__links__item">
+            <img src="{{ asset('src/img/icons/download/app-store.png')}}" alt="Available on the App Store">
+        </a>
+
+        <a target="_blank" href="https://play.google.com/store/games?hl=uk&gl=US" class="available__links__item">
+            <img src="{{ asset('src/img/icons/download/google-play.png')}}" alt="Get it on Google Play">
+        </a>
+    </div>
+  </section>
+
   <script>
     const dropdowns = document.querySelectorAll(".header__dropdown");
 
