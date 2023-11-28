a86d61f9a46728a705a264e1002d04f1c9d810df
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-11-28 14:55:34 +0200

    sales block created

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,3 +1,6 @@
+@php
+    $array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+@endphp
 <x-base>
     <header id="head" class="header d-flex flex-row justify-content-end">
         <ul class="header__list d-flex flex-row align-items-center">
@@ -1135,6 +1138,61 @@
         </div>
     </section>
 
+    <section class="sales d-flex flex-row align-items-center">
+        <div class="">
+            <h2 class="sales__header--token title-main title-main--stroke">
+                ICO Token
+            </h2>
+
+            <h2 class="sales__header title-main">
+                Sales?
+            </h2>
+
+            <h3 class="sales__desc title-little">
+                SALES INFO
+            </h3>
+        </div>
+
+        <div class="sales__block">
+            <div class="sales__block__btns d-flex flex-row justify-content-end align-items-center">
+                <img
+                    src="{{ asset('src/img/icons/arrow_right_short.svg')}}"
+                    alt="Arrow Left"
+                    class="
+                        sales__block__btns__img
+                        sales__block__btns__img--left
+                    "
+                    onclick="scrollCards(-1200)"
+                >
+
+                <img
+                    src="{{ asset('src/img/icons/arrow_right_short.svg')}}"
+                    alt="Arrow Right"
+                    class="
+                        sales__block__btns__img
+                        sales__block__btns__img--right
+                    "
+                    onclick="scrollCards(1200)"
+                >
+            </div>
+
+            <div class="sales__block__cards d-flex flex-row position-relative text-center">
+                @foreach($array as $key => $number)
+                    <div class="sales__block__cards__item">
+                        <div class="sales__block__cards__item__top">
+                            <p class="sales__block__cards__item__top__text desc">
+                                0.5% Bonus <br> $2009/token
+                            </p>
+                        </div>
+
+                        <h3 class="sales__block__cards__item__date title-little">
+                            Jul 08 - Jul 2{{ $number }}
+                        </h3>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
     <section class="available text-center">
         <h2 class="available__title title-main">
             Also Available <span class=" title-main--stroke">LevitAI</span> <br>
@@ -1223,6 +1281,8 @@
             });
         });
 
+        //face
+
         window.addEventListener('scroll', function() {
             const faceWhite = document.getElementById('face-white');
             const faceMain = document.getElementById('face-main');
@@ -1259,5 +1319,26 @@
                 faceColor.style.opacity = 0;
             }
         });
+
+        //sales
+
+        const cardsContainer = document.querySelector('.sales__block__cards');
+        const leftButton = document.querySelector('.sales__block__btns__img--left');
+        const rightButton = document.querySelector('.sales__block__btns__img--right');
+
+        function updateButtonPadding() {
+            const canScrollLeft = cardsContainer.scrollLeft > 0;
+            const canScrollRight = cardsContainer.scrollLeft < (cardsContainer.scrollWidth - cardsContainer.clientWidth);
+
+            leftButton.style.padding = canScrollLeft ? '25px 27px' : '18px 20px';
+            rightButton.style.padding = canScrollRight ? '25px 27px' : '18px 20px';
+        }
+
+        function scrollCards(scrollAmount) {
+            cardsContainer.scrollLeft += scrollAmount;
+        }
+
+        cardsContainer.addEventListener('scroll', updateButtonPadding);
+
     </script>
 </x-base>
