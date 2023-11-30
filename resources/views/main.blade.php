60f2993ab6f7a5f2a859b0aa63fcde5a5e1d61f4
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-11-30 20:21:08 +0200

    docs block created

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1764,6 +1764,126 @@
         </div>
     </section>
 
+    <section class="docs">
+        <div class="docs__top">
+            <h2 class="docs__top__title title-main">
+                Read <span class="title-main--stroke">LevitAI</span>
+            </h2>
+
+            <div class="docs__top__block d-flex flex-row align-items-center">
+                <p class="docs__top__block__desc desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+                </p>
+
+                <h2 class="docs__top__block__title title-main">
+                    Documents
+                </h2>
+            </div>
+        </div>
+
+        <div class="docs__content d-flex flex-row align-items-center justify-content-between">
+            <img src="{{ asset('src/img/photo/docs.png')}}" alt="Read LevitAI Documents" class="docs__content__img">
+
+            <div class="docs__content__list">
+                <div class="docs__content__list__item d-flex flex-row align-items-center">
+                    <img src="{{ asset('src/img/icons/docs-icon.png')}}" alt="First Doc" class="docs__content__list__item__img">
+
+                    <p class="docs__content__list__item__title desc">
+                        White Paper
+                    </p>
+
+                    <p class="docs__content__list__item__date desc">
+                        2021
+                    </p>
+
+                    <p class="docs__content__list__item__lang desc">
+                        English
+                    </p>
+
+                    <div class="docs__content__list__item__line"></div>
+
+                    <button class="docs__content__list__item__btn d-flex flex-row align-items-center">
+                        <img src="{{ asset('src/img/icons/arrow-download.svg')}}" alt="Download file" class="docs__content__list__item__btn__img">
+
+                        Document
+                    </button>
+                </div>
+
+                <div class="docs__content__list__item d-flex flex-row align-items-center">
+                    <img src="{{ asset('src/img/icons/docs-icon.png')}}" alt="First Doc" class="docs__content__list__item__img">
+
+                    <p class="docs__content__list__item__title desc">
+                        White Paper
+                    </p>
+
+                    <p class="docs__content__list__item__date desc">
+                        2021
+                    </p>
+
+                    <p class="docs__content__list__item__lang desc">
+                        English
+                    </p>
+
+                    <div class="docs__content__list__item__line"></div>
+
+                    <button class="docs__content__list__item__btn d-flex flex-row align-items-center">
+                        <img src="{{ asset('src/img/icons/arrow-download.svg')}}" alt="Download file" class="docs__content__list__item__btn__img">
+
+                        Document
+                    </button>
+                </div>
+
+                <div class="docs__content__list__item d-flex flex-row align-items-center">
+                    <img src="{{ asset('src/img/icons/docs-icon.png')}}" alt="First Doc" class="docs__content__list__item__img">
+
+                    <p class="docs__content__list__item__title desc">
+                        White Paper
+                    </p>
+
+                    <p class="docs__content__list__item__date desc">
+                        2021
+                    </p>
+
+                    <p class="docs__content__list__item__lang desc">
+                        English
+                    </p>
+
+                    <div class="docs__content__list__item__line"></div>
+
+                    <button class="docs__content__list__item__btn d-flex flex-row align-items-center">
+                        <img src="{{ asset('src/img/icons/arrow-download.svg')}}" alt="Download file" class="docs__content__list__item__btn__img">
+
+                        Document
+                    </button>
+                </div>
+
+                <div class="docs__content__list__item d-flex flex-row align-items-center">
+                    <img src="{{ asset('src/img/icons/docs-icon.png')}}" alt="First Doc" class="docs__content__list__item__img">
+
+                    <p class="docs__content__list__item__title desc">
+                        White Paper
+                    </p>
+
+                    <p class="docs__content__list__item__date desc">
+                        2021
+                    </p>
+
+                    <p class="docs__content__list__item__lang desc">
+                        English
+                    </p>
+
+                    <div class="docs__content__list__item__line"></div>
+
+                    <button class="docs__content__list__item__btn d-flex flex-row align-items-center">
+                        <img src="{{ asset('src/img/icons/arrow-download.svg')}}" alt="Download file" class="docs__content__list__item__btn__img">
+
+                        Document
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <footer class="footer ">
         <div class="footer__top d-flex flex-row justify-content-end align-items-center">
             <p class="footer__top__desc text-end">
