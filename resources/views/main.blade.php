49a4bf2f90b86bdc2726d5bbfdd444b6d29cddc3
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-12-04 19:01:21 +0200

    roadmap anomation added

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1194,7 +1194,7 @@
         </div>
     </section>
 
-    <section class="roadmap">
+    <section id="roadmap" class="roadmap">
         <div class="roadmap__top d-flex flex-row justify-content-between">
             <div class="roadmap__top__text">
                 <h2 class="title-main">
@@ -1215,7 +1215,9 @@
             <img src="{{ asset('src/img/photo/choose-img.jpg')}}" alt="Our Strategy & Roadmap" class="roadmap__top__img">
         </div>
 
-        <img src="{{ asset('src/img/roadmap.png')}}" alt="" class="roadmap__img">
+        <img id="map" src="{{ asset('src/img//photo/roadmap/roadmap.png')}}" alt="" class="roadmap__img roadmap__img--map">
+
+        <img id="line" src="{{ asset('src/img//photo/roadmap/line.png')}}" alt="" class="roadmap__img roadmap__img--line">
     </section>
 
     <section class="available text-center">
@@ -2116,5 +2118,39 @@
                 });
             });
         })
+
+        //roadmap
+        window.addEventListener('scroll', function() {
+            const line = document.getElementById('line');
+            const map = document.getElementById('map');
+            const scrollPosition = window.scrollY;
+
+            const triggerDiv = document.getElementById('roadmap');
+            const triggerDivPosition = triggerDiv.offsetTop;
+
+            const opacity = 0.79 - (scrollPosition - triggerDivPosition) / triggerDiv.clientHeight;
+
+            const limitedOpacity = Math.min(1, Math.max(0, opacity));
+
+            console.log(limitedOpacity);
+
+            if (limitedOpacity > 0.85 || limitedOpacity <= 0.001) {
+                line.style.opacity = 1;
+                map.style.opacity = 0;
+
+                setTimeout(() => {
+                    map.style.display = 'none';
+                    line.style.display = 'block';
+                }, 1800);
+            }else {
+                line.style.opacity = 0;
+                map.style.opacity = 1;
+
+                setTimeout(() => {
+                    line.style.display = 'none';
+                    map.style.display = 'block';
+                }, 1800);
+            }
+        });
     </script>
 </x-base>
