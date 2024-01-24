3eeb71a627045b155adeea543a298458df465561
Anastasiia Fursyk <anastasianauk44@gmail.com>
2024-01-24 16:36:10 +0200

    messages page creating

--- resources/views/components/account/messages.blade.php
+++ resources/views/components/account/messages.blade.php
@@ -3,12 +3,12 @@
         'dialog1'=> [
             'id' => 'fri1',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => true,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'mine',
-            'lastStatus' => 'unread'
+            'lastStatus' => 'unread',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -45,12 +45,12 @@
         'dialog2'=> [
             'id' => 'fri2',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => false,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'notMine',
-            'lastStatus' => 'read'
+            'lastStatus' => 'read',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -87,12 +87,12 @@
         'dialog3'=> [
             'id' => 'new1',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => true,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'mine',
-            'lastStatus' => 'read'
+            'lastStatus' => 'read',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -129,12 +129,12 @@
         'dialog4'=> [
             'id' => 'new2',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => true,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'mine',
-            'lastStatus' => 'unread'
+            'lastStatus' => 'unread',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -171,12 +171,12 @@
         'dialog5'=> [
             'id' => 'gro1',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => true,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'notMine',
-            'lastStatus' => 'read'
+            'lastStatus' => 'read',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -213,12 +213,12 @@
         'dialog6'=> [
             'id' => 'gro2',
             'name' => 'Artem Jonson',
-            'photo' => 'src/img/photo/nft/user1.jpg'
+            'photo' => 'src/img/photo/nft/user1.jpg',
             'online' => false,
             'lastDate' => '13.02.2023',
             'lastTime' => '12:47',
             'lastMessage' => 'mine',
-            'lastStatus' => 'read'
+            'lastStatus' => 'read',
             'messages' => [
                 'message1' => [
                     'id' => 1,
@@ -258,136 +258,139 @@
 <x-base>
     <x-menu />
 
-    <x-header />
-    <section class="messages d-flex flex-row">
-        <div class="messages__filter">
-            <div class="messages__filter__item">
-                <div class="messages__filter__item__top">
-                    <img src="{{ asset('src/img/icons/messages/all.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+    <section class="messages">
+        <x-header />
 
-                    <div class="messages__filter__item__top__num">
-                        <p class="messages__filter__item__top__num__text">
-                            2
-                        </p>
-                    </div>
-                </div>
+        <div class="messages__content d-flex flex-row">
+            <div class="messages__filter d-flex flex-column align-items-center text-center">
+                <div class="messages__filter__item active">
+                    <div class="messages__filter__item__top">
+                        <img src="{{ asset('src/img/icons/messages/all.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
 
-                <p class="messages__filter__item__title desc">
-                    All <br> Messages
-                </p>
-            </div>
-
-            <div class="messages__filter__item">
-                <div class="messages__filter__item__top">
-                    <img src="{{ asset('src/img/icons/messages/new.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
-
-                    <div class="messages__filter__item__top__num">
-                        <p class="messages__filter__item__top__num__text">
-                            2
-                        </p>
-                    </div>
-                </div>
-
-                <p class="messages__filter__item__title desc">
-                    New <br> Messages
-                </p>
-            </div>
-
-            <div class="messages__filter__item">
-                <div class="messages__filter__item__top">
-                    <img src="{{ asset('src/img/icons/messages/user.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
-
-                    <div class="messages__filter__item__top__num">
-                        <p class="messages__filter__item__top__num__text">
-                            2
-                        </p>
-                    </div>
-                </div>
-
-                <p class="messages__filter__item__title desc">
-                    My <br> Friends
-                </p>
-            </div>
-
-            <div class="messages__filter__item">
-                <div class="messages__filter__item__top">
-                    <img src="{{ asset('src/img/icons/messages/group.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
-
-                    <div class="messages__filter__item__top__num">
-                        <p class="messages__filter__item__top__num__text">
-                            2
-                        </p>
-                    </div>
-                </div>
-
-                <p class="messages__filter__item__title desc">
-                    Group <br> Messages
-                </p>
-            </div>
-
-            <div class="messages__filter__item">
-                <div class="messages__filter__item__top">
-                    <img src="{{ asset('src/img/icons/messages/settings.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
-
-                    <div class="messages__filter__item__top__num">
-                        <p class="messages__filter__item__top__num__text">
-                            2
-                        </p>
-                    </div>
-                </div>
-
-                <p class="messages__filter__item__title desc">
-                    Support <br> team
-                </p>
-            </div>
-        </div>
-
-        <div class="messages__dialogs">
-            <div class="messages__dialogs__top d-flex flex-row align-items-center justify-content-between">
-                <h3 class="messages__dialogs__top__title title-main">
-                    My Messages
-                </h3>
-
-                <div class="messages__dialogs__top__btns d-flex flex-row align-items-center">
-                  <a href="#" class="messages__dialogs__top__btns__search">
-                      <img src="{{ asset('src/img/icons/search.svg')}}" alt="Search" class="messages__dialogs__top__search__img">
-                  </a>
-
-                  <a href="#" class="messages__dialogs__top__btns__add">
-                      <img src="{{ asset('src/img/icons/add.svg')}}" alt="Add" class="messages__dialogs__top__add__img">
-                  </a>
-                </div>
-
-            </div>
-
-            <div class="messages__dialogs__item d-flex flex-row align-items-center">
-                <img src="" alt="Photo" class="messages__dialogs__item__img">
-
-                <div class="messages__dialogs__item__content">
-                    <div class="messages__dialogs__item__content__top d-flex flex-row justify-content-between align-items-center">
-                        <h4 class="messages__dialogs__item__content__top__name title-little">
-
-                        </h4>
-
-                        <p class="messages__dialogs__item__content__top__time desc">
-
-                        </p>
+                        <div class="messages__filter__item__top__num d-flex justify-content-center align-items-center">
+                            <p class="messages__filter__item__top__num__text">
+                                2
+                            </p>
+                        </div>
                     </div>
 
-                    <div class="messages__dialogs__item__content__bottom d-flex flex-row justify-content-between align-items-center">
-                        <p class="messages__dialogs__item__content__bottom__text desc">
+                    <p class="messages__filter__item__title desc">
+                        All <br> Messages
+                    </p>
+                </div>
 
-                        </p>
+                <div class="messages__filter__item">
+                    <div class="messages__filter__item__top">
+                        <img src="{{ asset('src/img/icons/messages/new.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
 
-                        <img src="" alt="Read it" class="messages__dialogs__item__content__bottom__check">
+                        <div class="messages__filter__item__top__num d-flex justify-content-center align-items-center">
+                            <p class="messages__filter__item__top__num__text">
+                                2
+                            </p>
+                        </div>
+                    </div>
+
+                    <p class="messages__filter__item__title desc">
+                        New <br> Messages
+                    </p>
+                </div>
+
+                <div class="messages__filter__item">
+                    <div class="messages__filter__item__top">
+                        <img src="{{ asset('src/img/icons/messages/user.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                        <div class="messages__filter__item__top__num d-flex justify-content-center align-items-center">
+                            <p class="messages__filter__item__top__num__text">
+                                2
+                            </p>
+                        </div>
+                    </div>
+
+                    <p class="messages__filter__item__title desc">
+                        My <br> Friends
+                    </p>
+                </div>
+
+                <div class="messages__filter__item">
+                    <div class="messages__filter__item__top">
+                        <img src="{{ asset('src/img/icons/messages/group.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                        <div class="messages__filter__item__top__num d-flex justify-content-center align-items-center">
+                            <p class="messages__filter__item__top__num__text">
+                                2
+                            </p>
+                        </div>
+                    </div>
+
+                    <p class="messages__filter__item__title desc">
+                        Group <br> Messages
+                    </p>
+                </div>
+
+                <div class="messages__filter__item">
+                    <div class="messages__filter__item__top">
+                        <img src="{{ asset('src/img/icons/messages/settings.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                        <div class="messages__filter__item__top__num d-flex justify-content-center align-items-center">
+                            <p class="messages__filter__item__top__num__text">
+                                2
+                            </p>
+                        </div>
+                    </div>
+
+                    <p class="messages__filter__item__title desc">
+                        Support <br> team
+                    </p>
+                </div>
+            </div>
+
+            <div class="messages__dialogs">
+                <div class="messages__dialogs__top d-flex flex-row align-items-center justify-content-between">
+                    <h3 class="messages__dialogs__top__title title-little">
+                        My Messages
+                    </h3>
+
+                    <div class="messages__dialogs__top__btns d-flex flex-row align-items-center">
+                      <a href="#" class="messages__dialogs__top__btns__search">
+                          <img src="{{ asset('src/img/icons/search.svg')}}" alt="Search" class="messages__dialogs__top__search__img">
+                      </a>
+
+                      <a href="#" class="messages__dialogs__top__btns__add">
+                          <img src="{{ asset('src/img/icons/messages/add.svg')}}" alt="Add" class="messages__dialogs__top__add__img">
+                      </a>
+                    </div>
+
+                </div>
+
+                <div class="messages__dialogs__item d-flex flex-row align-items-center">
+                    <img src="" alt="Photo" class="messages__dialogs__item__img">
+
+                    <div class="messages__dialogs__item__content">
+                        <div class="messages__dialogs__item__content__top d-flex flex-row justify-content-between align-items-center">
+                            <h4 class="messages__dialogs__item__content__top__name title-little">
+
+                            </h4>
+
+                            <p class="messages__dialogs__item__content__top__time desc">
+
+                            </p>
+                        </div>
+
+                        <div class="messages__dialogs__item__content__bottom d-flex flex-row justify-content-between align-items-center">
+                            <p class="messages__dialogs__item__content__bottom__text desc">
+
+                            </p>
+
+                            <img src="" alt="Read it" class="messages__dialogs__item__content__bottom__check">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="messages__chat">
-            <div class="messages__chat__top d-flex flex-row">
+            <div class="messages__chat">
+                <div class="messages__chat__top d-flex flex-row">
 
+                </div>
             </div>
         </div>
     </section>
