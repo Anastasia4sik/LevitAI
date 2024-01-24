231bc6f61e3b04d85e2c63180b17edde445174e3
Anastasiia Fursyk <anastasianauk44@gmail.com>
2024-01-24 15:47:23 +0200

    messages page creating

--- resources/views/components/account/messages.blade.php
+++ resources/views/components/account/messages.blade.php
@@ -1,4 +1,258 @@
 @php
+    $dialogs = [
+        'dialog1'=> [
+            'id' => 'fri1',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => true,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'mine',
+            'lastStatus' => 'unread'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+        'dialog2'=> [
+            'id' => 'fri2',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => false,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'notMine',
+            'lastStatus' => 'read'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+        'dialog3'=> [
+            'id' => 'new1',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => true,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'mine',
+            'lastStatus' => 'read'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+        'dialog4'=> [
+            'id' => 'new2',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => true,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'mine',
+            'lastStatus' => 'unread'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+        'dialog5'=> [
+            'id' => 'gro1',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => true,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'notMine',
+            'lastStatus' => 'read'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+        'dialog6'=> [
+            'id' => 'gro2',
+            'name' => 'Artem Jonson',
+            'photo' => 'src/img/photo/nft/user1.jpg'
+            'online' => false,
+            'lastDate' => '13.02.2023',
+            'lastTime' => '12:47',
+            'lastMessage' => 'mine',
+            'lastStatus' => 'read'
+            'messages' => [
+                'message1' => [
+                    'id' => 1,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message2' => [
+                    'id' => 2,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+                'message3' => [
+                    'id' => 3,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. Seamlessly generate user friendly opportunitie after principle-centered e-markets. Proactively visualize functional',
+                ],
+                'message4' => [
+                    'id' => 4,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Proactively visualize functional',
+                ],
+                'message5' => [
+                    'id' => 5,
+                    'time' => '17:00',
+                    'date' => '13.02.2023',
+                    'content' => 'Monotonectally productivate virtual benefits vis-a-vis clicks-and-mortar lead ship. ',
+                ],
+            ],
+        ],
+    ]
 @endphp
 
 <x-base>
@@ -9,30 +263,101 @@
         <div class="messages__filter">
             <div class="messages__filter__item">
                 <div class="messages__filter__item__top">
-                    <img src="" alt="Image of the block of messages" class="messages__filter__item__top__img">
+                    <img src="{{ asset('src/img/icons/messages/all.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
 
-                    <img src="" alt="Number of messages" class="messages__filter__item__top__num">
+                    <div class="messages__filter__item__top__num">
+                        <p class="messages__filter__item__top__num__text">
+                            2
+                        </p>
+                    </div>
                 </div>
 
                 <p class="messages__filter__item__title desc">
+                    All <br> Messages
+                </p>
+            </div>
 
+            <div class="messages__filter__item">
+                <div class="messages__filter__item__top">
+                    <img src="{{ asset('src/img/icons/messages/new.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                    <div class="messages__filter__item__top__num">
+                        <p class="messages__filter__item__top__num__text">
+                            2
+                        </p>
+                    </div>
+                </div>
+
+                <p class="messages__filter__item__title desc">
+                    New <br> Messages
+                </p>
+            </div>
+
+            <div class="messages__filter__item">
+                <div class="messages__filter__item__top">
+                    <img src="{{ asset('src/img/icons/messages/user.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                    <div class="messages__filter__item__top__num">
+                        <p class="messages__filter__item__top__num__text">
+                            2
+                        </p>
+                    </div>
+                </div>
+
+                <p class="messages__filter__item__title desc">
+                    My <br> Friends
+                </p>
+            </div>
+
+            <div class="messages__filter__item">
+                <div class="messages__filter__item__top">
+                    <img src="{{ asset('src/img/icons/messages/group.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                    <div class="messages__filter__item__top__num">
+                        <p class="messages__filter__item__top__num__text">
+                            2
+                        </p>
+                    </div>
+                </div>
+
+                <p class="messages__filter__item__title desc">
+                    Group <br> Messages
+                </p>
+            </div>
+
+            <div class="messages__filter__item">
+                <div class="messages__filter__item__top">
+                    <img src="{{ asset('src/img/icons/messages/settings.svg')}}" alt="Image of the block of messages" class="messages__filter__item__top__img">
+
+                    <div class="messages__filter__item__top__num">
+                        <p class="messages__filter__item__top__num__text">
+                            2
+                        </p>
+                    </div>
+                </div>
+
+                <p class="messages__filter__item__title desc">
+                    Support <br> team
                 </p>
             </div>
         </div>
 
         <div class="messages__dialogs">
-            <div class="messages__dialogs__top d-flex flex-row align-items-center">
+            <div class="messages__dialogs__top d-flex flex-row align-items-center justify-content-between">
                 <h3 class="messages__dialogs__top__title title-main">
-
+                    My Messages
                 </h3>
 
-                <div class="messages__dialogs__top__search">
-                    <img src="" alt="Search" class="messages__dialogs__top__search__img">
+                <div class="messages__dialogs__top__btns d-flex flex-row align-items-center">
+                  <a href="#" class="messages__dialogs__top__btns__search">
+                      <img src="{{ asset('src/img/icons/search.svg')}}" alt="Search" class="messages__dialogs__top__search__img">
+                  </a>
+
+                  <a href="#" class="messages__dialogs__top__btns__add">
+                      <img src="{{ asset('src/img/icons/add.svg')}}" alt="Add" class="messages__dialogs__top__add__img">
+                  </a>
                 </div>
 
-                <div class="messages__dialogs__top__add">
-                    <img src="" alt="Add" class="messages__dialogs__top__add__img">
-                </div>
             </div>
 
             <div class="messages__dialogs__item d-flex flex-row align-items-center">
@@ -62,7 +387,7 @@
 
         <div class="messages__chat">
             <div class="messages__chat__top d-flex flex-row">
-                
+
             </div>
         </div>
     </section>
