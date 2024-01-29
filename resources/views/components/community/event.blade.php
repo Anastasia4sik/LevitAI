67b3f71e83638cd796855b543ee2331255cae96e
Anastasiia Fursyk <anastasianauk44@gmail.com>
2024-01-29 14:49:37 +0200

    event page for mobile version changed

--- resources/views/components/community/event.blade.php
+++ resources/views/components/community/event.blade.php
@@ -1,152 +1,152 @@
 @php
-$event = [
-'event1' => [
-'id' => 1,
-'photo' => 'src/img/photo/events/event1.png',
-'title' => 'Lorem ipsum dolor sit amet',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-'date' => '12 Dec 2023',
-'time' => '12:46 pm',
-],
-'event2' => [
-'id' => 2,
-'photo' => 'src/img/photo/events/event2.png',
-'title' => 'Lorem ipsum dolor sit amet',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-'date' => '12 Dec 2023',
-'time' => '12:46 pm',
-],
-'event3' => [
-'id' => 3,
-'photo' => 'src/img/photo/events/event3.png',
-'title' => 'Lorem ipsum dolor sit amet',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-'date' => '12 Dec 2023',
-'time' => '12:46 pm',
-],
-'event4' => [
-'id' => 4,
-'photo' => 'src/img/photo/events/event2.png',
-'title' => 'Lorem ipsum dolor sit amet',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-'date' => '12 Dec 2023',
-'time' => '12:46 pm',
-],
-'event5' => [
-'id' => 5,
-'photo' => 'src/img/photo/events/event1.png',
-'title' => 'Lorem ipsum dolor sit amet',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-'date' => '12 Dec 2023',
-'time' => '12:46 pm',
-],
-];
+    $event = [
+        'event1' => [
+        'id' => 1,
+        'photo' => 'src/img/photo/events/event1.png',
+        'title' => 'Lorem ipsum dolor sit amet',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'date' => '12 Dec 2023',
+        'time' => '12:46 pm',
+        ],
+        'event2' => [
+        'id' => 2,
+        'photo' => 'src/img/photo/events/event2.png',
+        'title' => 'Lorem ipsum dolor sit amet',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'date' => '12 Dec 2023',
+        'time' => '12:46 pm',
+        ],
+        'event3' => [
+        'id' => 3,
+        'photo' => 'src/img/photo/events/event3.png',
+        'title' => 'Lorem ipsum dolor sit amet',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'date' => '12 Dec 2023',
+        'time' => '12:46 pm',
+        ],
+        'event4' => [
+        'id' => 4,
+        'photo' => 'src/img/photo/events/event2.png',
+        'title' => 'Lorem ipsum dolor sit amet',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'date' => '12 Dec 2023',
+        'time' => '12:46 pm',
+        ],
+        'event5' => [
+        'id' => 5,
+        'photo' => 'src/img/photo/events/event1.png',
+        'title' => 'Lorem ipsum dolor sit amet',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'date' => '12 Dec 2023',
+        'time' => '12:46 pm',
+        ],
+        ];
 
-$commentInfo = [
-'item1' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:47',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 0,
-],
-'item2' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 1,
-],
-'item3' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized
-by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are
-bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as
-saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
-when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every
-pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the
-obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise
-man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater
-pleasures, or else he endures pains to avoid worse pains.',
-'position' => 0,
-],
-'item4' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 0,
-],
-'item5' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:47',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 0,
-],
-'item6' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 1,
-],
-'item7' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized
-by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are
-bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as
-saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
-when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every
-pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the
-obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise
-man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater
-pleasures, or else he endures pains to avoid worse pains.',
-'position' => 2,
-],
-'item8' => [
-'photo' => 'src/img/photo/comment.jpg',
-'name' => 'Artem Jonson',
-'date' => '10.12.2023',
-'time' => '13:59',
-'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-'position' => 0,
-]
-];
+        $commentInfo = [
+        'item1' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:47',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 0,
+        ],
+        'item2' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 1,
+        ],
+        'item3' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized
+        by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are
+        bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as
+        saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
+        when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every
+        pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the
+        obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise
+        man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater
+        pleasures, or else he endures pains to avoid worse pains.',
+        'position' => 0,
+        ],
+        'item4' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 0,
+        ],
+        'item5' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:47',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 0,
+        ],
+        'item6' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 1,
+        ],
+        'item7' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized
+        by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are
+        bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as
+        saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
+        when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every
+        pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the
+        obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise
+        man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater
+        pleasures, or else he endures pains to avoid worse pains.',
+        'position' => 2,
+        ],
+        'item8' => [
+        'photo' => 'src/img/photo/comment.jpg',
+        'name' => 'Artem Jonson',
+        'date' => '10.12.2023',
+        'time' => '13:59',
+        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit ese cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'position' => 0,
+        ]
+    ];
 @endphp
 
 <x-base>
@@ -157,23 +157,25 @@ cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est la
 
     <section class="article">
       <div class="article__path d-flex flex-row ">
-        <a href="/" class="article__path__item article__path__item--links title-little">
-          Home \&#160;
-        </a>
-        <a href="/blog" class="article__path__item article__path__item--links title-little">
-          Blog \&#160;
-        </a>
-        <p class="article__path__item title-little">
-
-          Cryptocash Is A Clean & Modern Coin Service - Levitai
+        <p class="article__path eventsSection__top__path__item title-little">
+            <span>
+                <a href="/" class="article__path article__path--links title-little">
+                    Home \&#160; </a>
+                </span>
+            <span>
+                <a href="/blog" class="article__path article__path--links title-little">
+                     Blog \&#160;
+                </a>
+            </span>
+            Cryptocash Is A Clean & Modern Coin Service - Levitai
         </p>
       </div>
 
-      <h2 class="article__title title-main">
+      <h2 class="article__title article__title--event title-main">
         Cryptocash Is A Clean & Modern Coin Service - Levitai
       </h2>
 
-      <div class="article__button disable d-flex flex-row align-items-center">
+      <div class="article__button disable d-flex">
         <a href="#" class="
                         complete__btn
                         d-flex
@@ -197,7 +199,7 @@ cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est la
         </p>
       </div>
 
-      <div class="article__info d-flex flex-row">
+      <div class="article__info d-flex">
         <div class="d-flex flex-row article__info__row">
           <img src="{{ asset('src/img/icons/event/star.svg')}}" alt="Events">
 
@@ -231,7 +233,7 @@ cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est la
         </div>
       </div>
 
-      <div class="article__content d-flex flex-row justify-content-between align-items-center">
+      <div class="article__content d-flex align-items-center">
         <img src="{{ asset('src/img/photo/blog.jpg')}}" alt="Cryptocash Is A Clean & Modern Coin Service - Levitai"
           class="article__content__img">
 
