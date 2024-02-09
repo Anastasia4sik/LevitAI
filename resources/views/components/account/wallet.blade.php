a69358cfa49374a90a78a94369e047f06f78b156
Anastasiia Fursyk <anastasianauk44@gmail.com>
2024-02-09 15:29:48 +0200

    wallet mage creating

--- resources/views/components/account/wallet.blade.php
+++ resources/views/components/account/wallet.blade.php
@@ -1,3 +1,497 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+@php
+    $referrals = [
+        'user1' => [
+            'id' => 1,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user2' => [
+            'id' => 2,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user3' => [
+            'id' => 3,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user4' => [
+            'id' => 4,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user5' => [
+            'id' => 5,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user6' => [
+            'id' => 6,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user7' => [
+            'id' => 7,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+        'user8' => [
+            'id' => 8,
+            'img' => 'src/img/photo/account/avatar-small.png',
+            'name' => 'Artem',
+            'surname' => 'Jonson',
+            'date' => '12.06.2023',
+            'info' => '+0.45753',
+        ],
+    ];
+
+    $account = [
+        'id' => 1,
+        'photoTop' => 'src/img/photo/account/avatar-top.png',
+        'photoSmall' => 'src/img/photo/account/avatar-small.jpg',
+        'photoEdit' => 'src/img/photo/account/avatar-edit.jpg',
+        'name' => 'Artem',
+        'surname' => 'Jonson',
+        'phone' => '+380 99 105 35 17',
+        'mail' => 'gp.ugspl@gmail.com',
+        'langs' => 'Ukraine, English, Polish',
+        'nationality' => 'Ukrainian',
+        'age' => '25',
+        'sex' => 'Man',
+        'hobby' => 'To take dance, to surf, to hike',
+        'music' => 'jazz, hip-hop, rock, blues, neoclassical',
+        'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'values' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'employment' => 'web design, graphic design, illustration',
+        'wallets' => 6,
+        'funds' => '12.454667',
+        'operations' => 38,
+    ];
+
+    $wallets = [
+        'wallet1' => [
+            'id' => '1',
+            'name' => 'Bitcoin',
+            'acronym' => 'BTC',
+            'icon' => '/src/img/icons/transactions/bitcoin.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet2' => [
+            'id' => '2',
+            'name' => 'Ethereum',
+            'acronym' => 'ETH',
+            'icon' => 'src/img/icons/transactions/ethereum.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet3' => [
+            'id' => '3',
+            'name' => 'Litecoin',
+            'acronym' => 'LTK',
+            'icon' => 'src/img/icons/transactions/L.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet4' => [
+            'id' => '4',
+            'name' => 'Bitcoin',
+            'acronym' => 'BTC',
+            'icon' => 'src/img/icons/transactions/bitcoin.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet5' => [
+            'id' => '5',
+            'name' => 'Ethereum',
+            'acronym' => 'ETH',
+            'icon' => 'src/img/icons/transactions/ethereum.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet6' => [
+            'id' => '6',
+            'name' => 'Litecoin',
+            'acronym' => 'LTK',
+            'icon' => 'src/img/icons/transactions/L.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet7' => [
+            'id' => '7',
+            'name' => 'Bitcoin',
+            'acronym' => 'BTC',
+            'icon' => 'src/img/icons/transactions/bitcoin.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet8' => [
+            'id' => '8',
+            'name' => 'Ethereum',
+            'acronym' => 'ETH',
+            'icon' => 'src/img/icons/transactions/ethereum.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet9' => [
+            'id' => '9',
+            'name' => 'Litecoin',
+            'acronym' => 'LTK',
+            'icon' => 'src/img/icons/transactions/L.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet10' => [
+            'id' => '10',
+            'name' => 'Bitcoin',
+            'acronym' => 'BTC',
+            'icon' => 'src/img/icons/transactions/bitcoin.svg',
+            'amount' => 0.236564,
+        ],
+        'walle11' => [
+            'id' => '11',
+            'name' => 'Ethereum',
+            'acronym' => 'ETH',
+            'icon' => 'src/img/icons/transactions/ethereum.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet12' => [
+            'id' => '12',
+            'name' => 'Litecoin',
+            'acronym' => 'LTK',
+            'icon' => 'src/img/icons/transactions/L.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet13' => [
+            'id' => '13',
+            'name' => 'Bitcoin',
+            'acronym' => 'BTC',
+            'icon' => 'src/img/icons/transactions/bitcoin.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet14' => [
+            'id' => '14',
+            'name' => 'Ethereum',
+            'acronym' => 'ETH',
+            'icon' => 'src/img/icons/transactions/ethereum.svg',
+            'amount' => 0.236564,
+        ],
+        'wallet15' => [
+            'id' => '15',
+            'name' => 'Litecoin',
+            'acronym' => 'LTK',
+            'icon' => 'src/img/icons/transactions/L.svg',
+            'amount' => 0.236564,
+        ],
+    ];
+
+    $circles = [
+        'circle1' => [
+            'id' => '1',
+            'name' => 'Bitcoin',
+            'percentage' => 100,
+        ],
+        'circle2' => [
+            'id' => '2',
+            'name' => 'Ethereum',
+            'percentage' => 29,
+        ],
+        'circle3' => [
+            'id' => '3',
+            'name' => 'Litecoin',
+            'percentage' => 11,
+        ],
+        'circle4' => [
+            'id' => '4',
+            'name' => 'Bitcoin',
+            'percentage' => 60,
+        ],
+        'circle5' => [
+            'id' => '5',
+            'name' => 'Ethereum',
+            'percentage' => 29,
+        ],
+        'circle6' => [
+            'id' => '6',
+            'name' => 'Litecoin',
+            'percentage' => 11,
+        ],
+        'circle7' => [
+            'id' => '7',
+            'name' => 'Bitcoin',
+            'percentage' => 60,
+        ],
+        'circle8' => [
+            'id' => '8',
+            'name' => 'Ethereum',
+            'percentage' => 29,
+        ],
+        'circle9' => [
+            'id' => '9',
+            'name' => 'Litecoin',
+            'percentage' => 11,
+        ],
+        'circle10' => [
+            'id' => '10',
+            'name' => 'Bitcoin',
+            'percentage' => 60,
+        ],
+        'circle11' => [
+            'id' => '11',
+            'name' => 'Ethereum',
+            'percentage' => 29,
+        ],
+        'circle12' => [
+            'id' => '12',
+            'name' => 'Litecoin',
+            'percentage' => 11,
+        ],
+        'circle13' => [
+            'id' => '13',
+            'name' => 'Bitcoin',
+            'percentage' => 60,
+        ],
+        'circle14' => [
+            'id' => '14',
+            'name' => 'Ethereum',
+            'percentage' => 29,
+        ],
+        'circle15' => [
+            'id' => '15',
+            'name' => 'Litecoin',
+            'percentage' => 11,
+        ],
+    ]
+@endphp
+
+<x-base>
+    <section class="wallet">
+        <x-menu />
+
+        <x-header />
+
+        <div class="wallet__content d-flex">
+            <div class="wallet__content__top">
+                <div class="wallet__content__top__row d-flex">
+                    <h1 class="wallet__content__top__title title-main">
+                        My <span class="wallet__content__top__title--span title-main title-main--stroke">Wallet</span>
+                    </h1>
+
+                    <p class="wallet__content__top__desc desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt labore dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    </p>
+                </div>
+
+                <h1 class="wallet__content__top__title wallet__content__top__title--desk title-main title-main--stroke">
+                    Wallet
+                </h1>
+            </div>
+
+            <div class="wallet__content__big d-flex">
+                <div class="wallet__content__wallets">
+                    <div class="wallet__content__wallets__top d-flex flex-row align-items-center justify-content-between">
+                        <h4 class="wallet__content__wallets__top__title title-little">
+                            My Wallets
+                        </h4>
+
+                        <a href="#" target="_blank" class="wallet__content__wallets__top__link desc">
+                            Read more
+                        </a>
+                    </div>
+
+                    <div class="wallet__content__wallets__list d-flex flex-row">
+                        @foreach ($wallets as $wallet)
+                            <div class="wallet__content__wallets__list__item d-flex flex-column justify-content-between">
+                                <div class="wallet__content__wallets__list__item__top d-flex flex-row align-items-center justify-content-between">
+                                    <p class="wallet__content__wallets__list__item__top__acronym title-little">
+                                        {{ $wallet['acronym']}}
+                                    </p>
+
+                                    <img src="{{ asset($wallet['icon'])}}" alt="Wallet icon" class="wallet__content__wallets__list__item__top__icon">
+                                </div>
+
+                                <div class="wallet__content__wallets__list__item__bottom d-flex flex-row align-items-center justify-content-between">
+                                    <p class="wallet__content__wallets__list__item__bottom__name title-little">
+                                        {{ $wallet['name']}}
+                                    </p>
+
+                                    <p class="wallet__content__wallets__list__item__bottom__amount title-little">
+                                        {{ $wallet['amount']}}
+                                    </p>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+
+                <div class="wallet__content__info wallet__content__info--mob">
+                    <div class="wallet__content__info__top d-flex flex-row justify-content-between">
+                        <div class="wallet__content__info__top__transactions d-flex flex-column">
+                            <p class="wallet__content__info__top__transactions__nums title-little">
+                                2.354
+                            </p>
+
+                            <p class="wallet__content__info__top__transactions__name title-little">
+                                Transactions
+                            </p>
+                        </div>
+
+                        <p class="wallet__content__info__top__wallets title-little">
+                            <span>5</span> <br> Wallet
+                        </p>
+                    </div>
+
+                    <div class="wallet__content__info__current d-flex flex-column">
+                        <p class="wallet__content__info__current__name title-little">
+                            Current Balance
+                        </p>
+
+                        <p class="wallet__content__info__current__amount title-little d-flex align-items-center">
+                            4.3440 <span class="wallet__content__info__current__amount__span title-little">USD</span>
+                        </p>
+                    </div>
+
+                    <div class="wallet__content__info__bottom d-flex flex-row justify-content-end">
+                        <p class="wallet__content__info__bottom__num desc">
+                            2.7994
+                        </p>
+
+                        <p class="wallet__content__info__bottom__cur desc">
+                            EUR
+                        </p>
+
+                        <p class="wallet__content__info__bottom__perc desc">
+                            +12%
+                        </p>
+                    </div>
+                </div>
+
+                <div class="wallet__content__percentages d-flex">
+                    <div class="wallet__content__circles d-flex flex-row">
+                        @foreach ($circles as $circle)
+                        @php
+                            $back = 360 - $circle['percentage'];
+                        @endphp
+                            <div class="wallet__content__circles__item d-flex flex-column align-items-center">
+                                <div class="wallet__content__circles__item__circle">
+                                    <div
+                                        class="wallet__content__circles__item__circle__radial progress-{{ $circle['percentage']}}"
+                                        style="background-image: linear-gradient(90deg, #4A10EF {{$circle['percentage']}}%, #2f3439 {{$circle['percentage']}}%, #2f3439 {{$back}}%);"
+                                    >
+                                        <div class="wallet__content__circles__item__circle__radial__overlay"></div>
+                                    </div>
+                                </div>
+
+                                <p class="wallet__content__circles__item__amount desc">
+                                    {{ $circle['percentage']}}%
+                                </p>
+
+                                <p class="wallet__content__circles__item__name title-little">
+                                    {{ $circle['name']}}
+                                </p>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="wallet__content__circles d-flex flex-row">
+                        @foreach ($circles as $circle)
+                        @php
+                            $back = 360 - $circle['percentage'];
+                        @endphp
+                            <div class="wallet__content__circles__item d-flex flex-column align-items-center">
+                                <div class="wallet__content__circles__item__circle">
+                                    <div
+                                        class="wallet__content__circles__item__circle__radial progress-{{ $circle['percentage']}}"
+                                        style="background-image: linear-gradient(90deg, #4A10EF {{$circle['percentage']}}%, #2f3439 {{$circle['percentage']}}%, #2f3439 {{$back}}%);"
+                                    >
+                                        <div class="wallet__content__circles__item__circle__radial__overlay"></div>
+                                    </div>
+                                </div>
+
+                                <p class="wallet__content__circles__item__amount desc">
+                                    {{ $circle['percentage']}}%
+                                </p>
+
+                                <p class="wallet__content__circles__item__name title-little">
+                                    {{ $circle['name']}}
+                                </p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+            <div class="wallet__content__info wallet__content__info--desk">
+                    <div class="wallet__content__info__top d-flex flex-row justify-content-between">
+                        <div class="wallet__content__info__top__transactions d-flex flex-column">
+                            <p class="wallet__content__info__top__transactions__nums title-little">
+                                2.354
+                            </p>
+
+                            <p class="wallet__content__info__top__transactions__name title-little">
+                                Transactions
+                            </p>
+                        </div>
+
+                        <p class="wallet__content__info__top__wallets title-little">
+                            <span>5</span> <br> Wallet
+                        </p>
+                    </div>
+
+                    <div class="wallet__content__info__current d-flex flex-column">
+                        <p class="wallet__content__info__current__name title-little">
+                            Current Balance
+                        </p>
+
+                        <p class="wallet__content__info__current__amount title-little d-flex align-items-center">
+                            4.3440 <span class="wallet__content__info__current__amount__span title-little">USD</span>
+                        </p>
+                    </div>
+
+                    <div class="wallet__content__info__bottom d-flex flex-row justify-content-end">
+                        <p class="wallet__content__info__bottom__num desc">
+                            2.7994
+                        </p>
+
+                        <p class="wallet__content__info__bottom__cur desc">
+                            EUR
+                        </p>
+
+                        <p class="wallet__content__info__bottom__perc desc">
+                            +12%
+                        </p>
+                    </div>
+                </div>
+        </div>
+    </section>
+
+    <x-transactions />
+
+    <x-footer />
+
+    <script>
+        const walletsListItems = document.querySelectorAll('.wallet__content__wallets__list__item');
+
+        walletsListItems.forEach((item) => {
+            item.addEventListener('click', () => {
+                walletsListItems.forEach((otherItem) => {
+                    otherItem.classList.remove('active');
+                });
+
+                item.classList.add('active');
+            });
+        });
+    </script>
+</x-base>
